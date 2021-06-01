5f1f254402b4b8b75381ebdb309b7cef918a8c99
Tommaso Stocchi <tommasostocchi@outlook.com>
2021-06-01 21:23:01 +0200

    -fix

--- src/function/Startup.cs
+++ src/function/Startup.cs
@@ -11,19 +11,10 @@ namespace FunctionApp
     {
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
         {
-            // Add Azure App Configuration as additional configuration source
-            // builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
-            // {
-            //     options.Connect(Environment.GetEnvironmentVariable("ConfigConnectionString"))
-            //            // Configure to reload configuration if the registered 'Sentinel' key is modified
-            //            .ConfigureRefresh(refreshOptions =>
-            //                 refreshOptions.Register("MockAPI:Sentinel", refreshAll: true)
-            //             )
-            //            .UseFeatureFlags();
-            // });
+            Add Azure App Configuration as additional configuration source
             builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
             {
-                options.Connect("Endpoint=https://mock-api-cfg.azconfig.io;Id=ro9L-lw-s0:He18bA4nn1Dr5Re2mvYa;Secret=WcmgiE/H1nUPnUNEkcwyFII0Z8J+xbUXMGEuBpGppSU=")
+                options.Connect(Environment.GetEnvironmentVariable("ConfigConnectionString"))
                        // Configure to reload configuration if the registered 'Sentinel' key is modified
                        .ConfigureRefresh(refreshOptions =>
                             refreshOptions.Register("MockAPI:Sentinel", refreshAll: true)
