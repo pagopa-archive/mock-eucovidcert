8278f5aed55b24aae425e7782dca096351ed3a3b
Michael Disaro <michael.disaro@gmail.com>
2021-06-08 15:42:05 +0200

    init refresh endpoint stub equal to certificate endpoint

--- src/function/Certificate.cs
+++ src/function/Certificate.cs
@@ -18,58 +18,149 @@ using System;
 using DGC.Models;
 using Microsoft.Extensions.Primitives;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
 
 namespace DGC.Function
 {
     public class Certificate
     {
+
         private const string AddedDelayConfigKey = "MockAPI:AddedDelay";
+
         private const string ForceStatusCodeFeature = "ForceStatusCode";
+
         private const string ForceStatusCodeKey = "MockAPI:ForceStatusCode";
+
         private const string FailureRateKey = "MockAPI:FailureRate";
+
         private const string CertificateThumbprintEnvVariable = "DGC_LOAD_TEST_CLIENT_KEY";
+
         private readonly IConfiguration _configuration;
+
         private readonly IConfigurationRefresher _configurationRefresher;
+
         private readonly IFeatureManagerSnapshot _featureManagerSnapshot;
 
-        public Certificate(IFeatureManagerSnapshot featureManagerSnapshot, IConfiguration configuration, IConfigurationRefresherProvider refresherProvider)
+        public Certificate(IFeatureManagerSnapshot featureManagerSnapshot,
+                           IConfiguration configuration,
+                           IConfigurationRefresherProvider refresherProvider)
         {
             _configuration = configuration;
             _featureManagerSnapshot = featureManagerSnapshot;
             _configurationRefresher = refresherProvider.Refreshers.First();
         }
 
-
         [FunctionName("Certificate")]
-        [OpenApiOperation(operationId: "getCertificate")]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "auth_code", Required = true, Type = typeof(string), Description = "The **auth_code** parameter")]
-        [OpenApiParameter(name: "fiscal_code", Required = true, Type = typeof(string), Description = "The **fiscal_code** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/certificate/citizen/io")] HttpRequest req,
-            ILogger log, ExecutionContext context)
+        [OpenApiOperation(operationId: "getCertificateByAutAndCF")]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code",
+                         In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "authCodeSHA256", Required = true, Type = typeof(string),
+                          Description = "The **auth_code** parameter")]
+        [OpenApiParameter(name: "cfSHA256", Required = true, Type = typeof(string),
+                          Description = "The **fiscal_code** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string),
+                                 Description = "The OK response")]
+        public async Task<IActionResult> GetCertificateByAutAndCf(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/certificate/citizen/io")]
+            HttpRequest req,
+            ILogger log,
+            ExecutionContext context)
         {
-            log.LogInformation("C# HTTP trigger function processed Certificate/getCertificate/Run");
+            log.LogInformation("C# HTTP trigger function processed Certificate/getCertificateByAutAndCF/GetCertificateByAutAndCf");
 
+            var errorMessage = await Init(req, log);
+            if (!errorMessage.IsNullOrWhiteSpace()) return new BadRequestObjectResult(errorMessage);
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string auth_code = data?.auth_code;
+            string fiscal_code = data?.fiscal_code;
+
+            var path = System.IO.Path.Combine(context.FunctionDirectory, "..\\payloads.json");
+            var payloadsJson = File.ReadAllText(path);
+            var payloads = JsonConvert.DeserializeObject<Payloads>(payloadsJson);
+            var random = new Random(DateTime.Now.Second);
+            var responseMessage = payloads.PayloadList[random.Next(0, payloads.PayloadList.Count - 1)];
+
+            var response = new ObjectResult(responseMessage);
+            if (await _featureManagerSnapshot.IsEnabledAsync(ForceStatusCodeFeature) &&
+                int.TryParse(_configuration[ForceStatusCodeKey], out int statusCode))
+            {
+                response.StatusCode = statusCode;
+            }
+            else
+            {
+                response.StatusCode = 200;
+            }
+
+            return response;
+
+        }
+
+        [FunctionName("Refresh")]
+        [OpenApiOperation(operationId: "managePreviousCertificates")]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code",
+                         In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "cfSHA256", Required = true, Type = typeof(string),
+                          Description = "The **fiscal_code** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string),
+                                 Description = "The OK response")]
+        public async Task<IActionResult> ManagePreviousCertificates(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/certificate/citizen/io/refresh")]
+            HttpRequest req,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation("C# HTTP trigger function processed Refresh/managePreviousCertificates/ManagePreviousCertificates");
+
+            var errorMessage = await Init(req, log);
+            if (!errorMessage.IsNullOrWhiteSpace()) return new BadRequestObjectResult(errorMessage);
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string fiscal_code = data?.fiscal_code;
+
+            var path = System.IO.Path.Combine(context.FunctionDirectory, "..\\payloads.json");
+            var payloadsJson = File.ReadAllText(path);
+            var payloads = JsonConvert.DeserializeObject<Payloads>(payloadsJson);
+            var random = new Random(DateTime.Now.Second);
+            var responseMessage = payloads.PayloadList[random.Next(0, payloads.PayloadList.Count - 1)];
+
+            var response = new ObjectResult(responseMessage);
+            if (await _featureManagerSnapshot.IsEnabledAsync(ForceStatusCodeFeature) &&
+                int.TryParse(_configuration[ForceStatusCodeKey], out int statusCode))
+            {
+                response.StatusCode = statusCode;
+            }
+            else
+            {
+                response.StatusCode = 200;
+            }
+
+            return response;
+        }
+
+        private async Task<string> Init(HttpRequest req,
+                                        ILogger log)
+        {
             //Mutual auth 
             if (req.Headers.TryGetValue("X-ARR-ClientCert", out StringValues cert))
             {
-
                 byte[] clientCertBytes = Convert.FromBase64String(cert[0]);
                 X509Certificate2 clientCert = new X509Certificate2(clientCertBytes);
 
                 // Validate Thumbprint
-                if (!clientCert.Thumbprint.Equals(Environment.GetEnvironmentVariable(CertificateThumbprintEnvVariable), StringComparison.InvariantCultureIgnoreCase))
+                if (!clientCert.Thumbprint.Equals(Environment.GetEnvironmentVariable(CertificateThumbprintEnvVariable),
+                                                  StringComparison.InvariantCultureIgnoreCase))
                 {
-                    return new BadRequestObjectResult("A valid client certificate is not used");
+                    return "A valid client certificate is not used";
                 }
 
                 // Validate NotBefore and NotAfter
                 if (DateTime.Compare(DateTime.UtcNow, clientCert.NotBefore) < 0
-                            || DateTime.Compare(DateTime.UtcNow, clientCert.NotAfter) > 0)
+                    || DateTime.Compare(DateTime.UtcNow, clientCert.NotAfter) > 0)
                 {
-                    return new BadRequestObjectResult("client certificate not in alllowed time interval");
+                    return "client certificate not in alllowed time interval";
                 }
 
                 // Add further validation of certificate if required.
@@ -95,29 +186,10 @@ namespace DGC.Function
                     await Task.Delay(addedDelay);
                 }
 
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                dynamic data = JsonConvert.DeserializeObject(requestBody);
-                string auth_code = data?.auth_code;
-                string fiscal_code = data?.fiscal_code;
-
-                var path = System.IO.Path.Combine(context.FunctionDirectory, "..\\payloads.json");
-                var payloadsJson = File.ReadAllText(path);
-                var payloads = JsonConvert.DeserializeObject<Payloads>(payloadsJson);
-                var random = new Random(DateTime.Now.Second);
-                var responseMessage = payloads.PayloadList[random.Next(0, payloads.PayloadList.Count - 1)];
-
-                var response = new ObjectResult(responseMessage);
-                if (await _featureManagerSnapshot.IsEnabledAsync(ForceStatusCodeFeature) && int.TryParse(_configuration[ForceStatusCodeKey], out int statusCode))
-                {
-                    response.StatusCode = statusCode;
-                }
-                else
-                {
-                    response.StatusCode = 200;
-                }
-                return response;
+                return null;
             }
-            return new BadRequestObjectResult("A valid client certificate is not found");
+
+            return "A valid client certificate is not found";
         }
 
     }
